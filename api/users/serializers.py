376c98089a8d1e0bf14ad8c336bf7bd9b9e5f00d
Raúl <raul@passporterapp.com>
2022-09-11 18:30:52 +0200

    FIX: Decrease min_length for phone serializer to match Landbot

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -5,7 +5,7 @@ from rest_framework.serializers import Serializer
 class CreateUserRequestSerializer(Serializer):
     name = CharField(min_length=2, max_length=128)
     email = EmailField()
-    phone = CharField(min_length=4, max_length=32)
+    phone = CharField(min_length=3, max_length=32)
     origin = CharField(
         min_length=2, max_length=32, allow_null=True, allow_blank=True, required=False
     )
