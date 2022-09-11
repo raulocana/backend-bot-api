8eefbe91c41d95626b30aaf7dfade70359ee3781
Raúl <raul@passporterapp.com>
2022-09-11 17:51:52 +0200

    FIX: Add allowance of null and blank values for origin serializer

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -6,4 +6,6 @@ class CreateUserRequestSerializer(Serializer):
     name = CharField(min_length=2, max_length=128)
     email = EmailField()
     phone = CharField(min_length=4, max_length=32)
-    origin = CharField(min_length=2, max_length=32, required=False)
+    origin = CharField(
+        min_length=2, max_length=32, allow_null=True, allow_blank=True, required=False
+    )
